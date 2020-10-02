a8d3021e7dc6281fd5ae76c3dd667b42a4c0728f
Linus Lee <linus@thesephist.com>
2020-10-02 04:18:42 -0400

    Add UI to make interactivity/editing more obvious

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,1 +1,9 @@
-// YOUR CODE GOES HERE: 
+// display all editable parts of the page
+
+function highlightEditables(evt) {
+    evt.preventDefault();
+    document.body.classList.add('highlightEditables');
+}
+
+const hleButton = document.querySelector('#highlightEditables');
+hleButton && hleButton.addEventListener('click', highlightEditables);
